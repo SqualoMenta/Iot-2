79fa36c5963a3915295ea4dd9e5cb4ebe57e1e48
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2024-11-18 15:00:27 +0100

    implemented a basic scheduler

---
+++ src/Scheduler.cpp
@@ -0,0 +1,35 @@
+#include "Scheduler.h"
+
+#include "Timer.h"
+
+volatile bool timerFlag;
+Timer timer;
+
+void timerHandler(void) { timerFlag = true; }
+
+void Scheduler::init(int basePeriod) {
+    this->basePeriod = basePeriod;
+    long period = 1000l * basePeriod;
+    timer.setupPeriod(period);
+    nTasks = 0;
+}
+
+bool Scheduler::addTask(Task* task) {
+    if (nTasks < MAX_TASKS - 1) {
+        taskList[nTasks] = task;
+        nTasks++;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Scheduler::schedule() {
+    timer.waitForNextTick();
+
+    for (int i = 0; i < nTasks; i++) {
+        if (taskList[i]->updateAndCheckTime(basePeriod)) {
+            taskList[i]->tick();
+        }
+    }
+}
